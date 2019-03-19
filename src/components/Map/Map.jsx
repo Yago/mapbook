f1cac1f03ab9d8d08693aeb5d427489ed52a9803
Yann Gouffon <github@yago-art.ch>
2019-03-19 20:45:36 +0100

    ✨ add marker filters

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,6 +10,8 @@ import {
   Marker,
   Popup,
   Pane,
+  LayersControl,
+  LayerGroup,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-fa-markers/L.Icon.FontAwesome.js';
@@ -39,11 +41,10 @@ const Map = ({}) => {
     );
   }
 
-  const [icons, setIcons] = useState([]);
-  if (icons.length <= 0) {
+  const [categories, setCategories] = useState([]);
+  if (categories.length <= 0) {
     airtableFetch('Categories').then(data =>
-      // data => console.log(data),
-      setIcons(
+      setCategories(
         data
           .filter(i => i.fields.name !== undefined)
           .map(cat => {
@@ -56,17 +57,13 @@ const Map = ({}) => {
             });
 
             return {
-              id: cat.id,
+              ...cat,
               icon,
             };
           }),
       ),
     );
   }
-
-  const corner1 = L.latLng(45.7769477403, 6.02260949059);
-  const corner2 = L.latLng(47.8308275417, 10.4427014502);
-  const bounds = L.latLngBounds(corner2, corner1);
 
   return (
     <LeafletMap
@@ -79,48 +76,72 @@ const Map = ({}) => {
       zoomSnap={true}
       zoomControl={false}
     >
-      <TileLayer
-        url="https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg"
-        detectRetina={true}
-        maxZoom={18}
-      />
-      <TileLayer
-        url="https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.swisstlm3d-wanderwege/default/current/3857/{z}/{x}/{y}.png"
-        detectRetina={true}
-      />
       <Pane name="fixedPane" />
-      {points.length > 0 &&
-        icons.length > 0 &&
-        points.map(point => (
-          <Marker
-            key={point.id}
-            position={[point.fields.latitude, point.fields.longitude]}
-            icon={icons.find(i => i.id === point.fields.category[0]).icon}
-          >
-            <Popup
-              autoPan={true}
-              minWidth={window.innerWidth - 50}
-              maxHeight={window.innerHeight - 200}
-              minHeight={window.innerHeight - 200}
-              offset={[0, window.innerHeight / 2]}
-              // keepInView
-              pane="fixedPane"
-              className="popup-fixed"
+      <LayersControl position="bottomright" collapsed={true}>
+        <LayersControl.BaseLayer name="Swiss color map" checked>
+          <TileLayer
+            url="https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg"
+            detectRetina={true}
+            maxZoom={18}
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.Overlay name="🗺️ Swiss hiking paths">
+          <TileLayer
+            url="https://wmts20.geo.admin.ch/1.0.0/ch.swisstopo.swisstlm3d-wanderwege/default/current/3857/{z}/{x}/{y}.png"
+            detectRetina={true}
+          />
+        </LayersControl.Overlay>
+        {categories.length > 0 &&
+          points.length > 0 &&
+          categories.map(category => (
+            <LayersControl.Overlay
+              key={category.id}
+              name={category.fields.name}
+              checked={category.fields.checked}
             >
-              {point.fields.title && <h3>{point.fields.title}</h3>}
-              {point.fields.description && (
-                <div
-                  dangerouslySetInnerHTML={{ __html: point.fields.description }}
-                />
-              )}
-              {point.fields.images &&
-                point.fields.images.length > 0 &&
-                point.fields.images.map(img => (
-                  <img key={img.id} src={img.thumbnails.large.url} />
-                ))}
-            </Popup>
-          </Marker>
-        ))}
+              <LayerGroup>
+                {points
+                  .filter(i => i.fields.category[0] === category.id)
+                  .sort((a, b) => b.fields.latitude - a.fields.latitude)
+                  .map(point => (
+                    <Marker
+                      key={point.id}
+                      position={[point.fields.latitude, point.fields.longitude]}
+                      icon={
+                        categories.find(i => i.id === point.fields.category[0])
+                          .icon
+                      }
+                    >
+                      <Popup
+                        autoPan={true}
+                        minWidth={window.innerWidth - 50}
+                        maxHeight={window.innerHeight - 200}
+                        minHeight={window.innerHeight - 200}
+                        offset={[0, window.innerHeight / 2]}
+                        pane="fixedPane"
+                        className="popup-fixed"
+                      >
+                        {point.fields.title && <h3>{point.fields.title}</h3>}
+                        {point.fields.description && (
+                          <div
+                            className="popup-desc"
+                            dangerouslySetInnerHTML={{
+                              __html: point.fields.description,
+                            }}
+                          />
+                        )}
+                        {point.fields.images &&
+                          point.fields.images.length > 0 &&
+                          point.fields.images.map(img => (
+                            <img key={img.id} src={img.thumbnails.large.url} />
+                          ))}
+                      </Popup>
+                    </Marker>
+                  ))}
+              </LayerGroup>
+            </LayersControl.Overlay>
+          ))}
+      </LayersControl>
     </LeafletMap>
   );
 };
--- src/components/Map/Map.styles.jsx
+++ src/components/Map/Map.styles.jsx
@@ -1,11 +1,15 @@
 import { css } from '@emotion/core';
 
 export default css`
+  @import url('https://fonts.googleapis.com/css?family=Merriweather:400,400i,700|Noto+Sans');
+
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
+  font-family: 'Noto Sans', Helvetica, Arial, sans-serif;
+  font-size: 0.9rem;
 
   .leaflet-fixed-pane {
     z-index: 800;
@@ -55,6 +59,59 @@ export default css`
       width: 20px;
       height: 20px;
       font-size: 20px;
+    }
+  }
+
+  .popup-desc {
+    font-family: 'Merriweather', Georgia, serif;
+    margin-bottom: 20px;
+  }
+
+  .leaflet-control-layers-expanded {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 25vw;
+    max-width: 300px;
+    min-height: calc(100vh - 35px);
+    border-radius: 0;
+
+    label input {
+      position: absolute;
+      z-index: -1;
+      opacity: 0;
+    }
+
+    label span {
+      display: block;
+      width: 90%;
+      margin-bottom: 5px;
+      padding: 10px 15px;
+      cursor: pointer;
+      transition: background 0.2s;
+      &:before {
+        content: '⬜';
+      }
+    }
+
+    label input[type='radio'] + span:before {
+      content: '⚪';
+    }
+
+    label input:checked + span {
+      background: #e0fadd;
+      &:before {
+        content: '✅';
+      }
+    }
+
+    label input[type='radio']:checked + span:before {
+      content: '🌏';
+    }
+
+    @media (max-width: 700px) {
+      width: calc(100vw - 130px);
+      max-width: calc(100vw - 130px);
     }
   }
 `;
